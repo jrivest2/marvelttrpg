8b8ab457de20b98f1922c77af9e0d5f9006f2fea
Justin Rivest <justin@Justin-Rivests-MacBook-Pro.local>
2023-11-28 19:17:17 -0700

    Made Initiative Modifier Clickable

--- src/components/InitModifier.js
+++ src/components/InitModifier.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function InitModifier({character}) {
     let initModifier = character.vigilance;
     let initEdge = "";
@@ -26,7 +28,32 @@ function InitModifier({character}) {
         }
     });
    
-    return <h3 className="subtitle">Initiative Modifier: +{initModifier} {initEdge}</h3>
+
+    const [isClicked, setIsClicked] = useState(false)
+
+    const handleClick = () => {
+        if (isClicked) setIsClicked(false)
+        else setIsClicked(true)
+    }
+
+    if (isClicked) {
+        return (
+            <div>
+                <div className="subtitle" onClick={handleClick}>Initiative Modifier: +{initModifier} {initEdge}</div>
+                <hr></hr>
+                <div>A character's <i>Initiative Modifier</i> helps determine when they can act during combat. The higher the number, the more likely they are to go first.</div>
+                <div>A character's Initiative Modifier is equal to their Vigilance. If there's an E next to a character's Initiative Modifier, that means they have an edge on their initiative checks.</div>
+                <br></br>
+                <div><b>Example</b></div>
+                <div><i>Spider-Man's Vigilance is 3, so his Initiative Modifier is +3 by default; however, Spider-Man also has a power called "Spider-Sense" that gives him an edge on initiative checks. So his Initiative modifier is +3 E</i></div>
+                <hr></hr>
+            </div>
+        );
+    }
+
+
+
+    return <h3 className="subtitle" onClick={handleClick}>Initiative Modifier: +{initModifier} {initEdge}</h3>
 };
 
 export default InitModifier;
