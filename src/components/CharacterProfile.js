5f025ce61fcc38f59fa5915a86bf6a2feb3e337b
Justin Rivest <justin@Justin-Rivests-MacBook-Pro.local>
2023-11-28 18:18:08 -0700

    Added Clickable Max Health and Max Focus

--- src/components/CharacterProfile.js
+++ src/components/CharacterProfile.js
@@ -14,8 +14,14 @@ function CharacterProfile({character, updateChar}) {
     let resilience = character.resilience;
     let vigilance = character.vigilance;
     
-    let maxHealth = 30 * resilience;
-    let maxFocus = 30 * vigilance;
+    let maxHealth = 0;
+    let maxFocus = 0;
+
+    if (resilience < 1) {maxHealth = 10;}
+    else {maxHealth = 30 * resilience;}
+    if (vigilance < 1) {maxFocus = 10;}
+    else {maxFocus = 30 * vigilance;}
+
     const [health, setHealth] = useState(30 * resilience)
     const [focus, setFocus] = useState(30 * vigilance)
     
@@ -106,6 +112,50 @@ function CharacterProfile({character, updateChar}) {
         func(character);
       }
     });
+
+    const [isMaxHealthClicked, setIsMaxHealthClicked] = useState(false)
+    const [isMaxFocusClicked, setIsMaxFocusClicked] = useState(false)
+
+    const handleMaxHealthClick = () => {
+      if (isMaxHealthClicked) setIsMaxHealthClicked(false)
+      else setIsMaxHealthClicked(true)
+    }
+
+    const handleMaxFocusClick = () => {
+      if (isMaxFocusClicked) setIsMaxFocusClicked(false)
+      else setIsMaxFocusClicked(true)
+    }
+
+    let maxHealthOutput = <p onClick={handleMaxHealthClick}>Max Health: {maxHealth}</p>
+    let maxFocusOutput = <p onClick={handleMaxFocusClick}>Max Focus: {maxFocus}</p>
+    
+    
+    if (isMaxHealthClicked) {
+      maxHealthOutput = (
+      <div onClick={handleMaxHealthClick}>
+        <div>Max Health: {maxHealth}</div>
+        <hr></hr>
+        <div>A character's <i>Health</i> measures their capacity to endure physical damage and keep fighting. It (Current Health) can be temporarily lowered by physical damage.</div>
+        <div>To calculate a character's Health, muliply their Resilience by 30. The minimum is 10 Health (Max Health), even if the character's Resilience is less than 1.</div>
+        <br></br>
+        <div><b>Example</b></div>
+        <div><i>Spider-Man's Resilience is 3. Multiply that by 30 to determine his Health, which is 90.</i></div>
+        <hr></hr>
+      </div>)
+    }
+    if (isMaxFocusClicked) {
+      maxFocusOutput = (
+      <div onClick={handleMaxFocusClick}>
+        <div>Max Focus: {maxFocus}</div>
+        <hr></hr>
+        <div>A character's <i>Focus</i> represents their capacity for concentration and willpower. It (Current Focus) can be temporarily lowered by psychic damage or the use of certain powers.</div>
+        <div>To calculate a character's Focus, muliply their Vigilance by 30. The minimum is 10 Focus (Max Focus), even if the character's Vigilance is less than 1.</div>
+        <br></br>
+        <div><b>Example</b></div>
+        <div><i>Spider-Man's Vigilance is 3. Multiply that by 30 to determine his Focus, which is 90.</i></div>
+        <hr></hr>
+      </div>)
+    }
     return (
       <section className='hero is-danger'>
         <div className='hero-body'>
@@ -114,10 +164,10 @@ function CharacterProfile({character, updateChar}) {
           <p className='title'>{name}</p>
           <p className='subtitle'>RANK: {rank}</p>
           <img src={ character.image } alt={"Image of " + character.name} />
-          <p>Max Health: {maxHealth}</p>
+          <div>{maxHealthOutput}</div>
           <TextBox onSubmit={handleHealthSubmit} starterText={"Current Health: " + health + " + " }/>
           <button style={{ backgroundColor: "", border: "0px solid lightgray", color:"", fontSize: "18px"}} onClick={resetHealth}>Reset Health</button>
-          <p>Max Focus: {maxFocus}</p>
+          <div>{maxFocusOutput}</div>
           <TextBox onSubmit={handleFocusSubmit} starterText={"Current Focus: " + focus + " + " }/>
           <button style={{ backgroundColor: "", border: "0px solid lightgray", color:"", fontSize: "18px"}} onClick={resetFocus}>Reset Focus</button>
 
